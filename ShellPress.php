b8f8c1cce43ce12b2c049f9fae3252046ff97ce5
dualjack <jakubkuranda@gmail.com>
2017-05-18 21:36:47 +0200

    Autloader oraz logger implementation test

--- ShellPress.php
+++ ShellPress.php
@@ -1,6 +1,9 @@
 <?php
 namespace shellpress_1_0_0;
 
+use Psr4AutoloaderClass;
+use Katzgrau\KLogger\Logger;
+
 
 
 
@@ -12,6 +15,8 @@ class ShellPress {
 
 	protected $options;
 	protected $views;
+	protected $autoloader;
+	protected $log;
 
 
     /**
@@ -20,8 +25,27 @@ class ShellPress {
 
 	function init( Array $args ) {
 
+        /**
+         * First of all - include and register autoloader
+         */
+        if( ! class_exists( 'Psr4AutoloaderClass' ) ){
+
+            require( __DIR__ . '/lib/external/Psr4AutoloaderClass.php' );
+
+        }
+
+        $this->autoloader = new Psr4AutoloaderClass();
+        $this->autoloader->register();
+        $this->autoloader->addNamespace( 'shellpress_1_0_0', __DIR__ );
+
+
 	    $this->options = new Options( $this );
 	    $this->options->init( array() );
+
+	    $this->autoloader->addNamespace( 'Katzgrau\KLogger', __DIR__ . '/lib/external/KLogger' );
+	    $this->autoloader->addNamespace( 'Psr\Log', __DIR__ . '/lib/external/Psr/Log' );
+	    $this->log = new Logger( __DIR__, \Psr\Log\LogLevel::DEBUG );
+
 
 		
 	}
