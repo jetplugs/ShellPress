500f8d0e4b76bd4b5a8833f9916ae04d0b89247d
dualjack <jakubkuranda@gmail.com>
2017-06-19 15:37:34 +0200

    Logger class now returns formated html string

--- src/Logger.php
+++ src/Logger.php
@@ -20,7 +20,15 @@ class Logger extends KLogger {
 
     }
 
-    public function getLog( $isAscending = false, $filePath = null ) {
+    /**
+     * Returns HTML string log of current day.
+     *
+     * @param bool $isAscending - if true, reverse rows
+     * @param null $filePath
+     *
+     * @return bool|string - returns formatted log string or false when error
+     */
+    public function getLog( $isAscending = true, $filePath = null ) {
 
         if( $filePath === null ){
 
@@ -30,7 +38,23 @@ class Logger extends KLogger {
 
         $fileContents = file_get_contents( $filePath );
 
-        return $fileContents;
+        if( $fileContents ){
+
+            $rows = explode( PHP_EOL, $fileContents );  //  explode file to rows by \n ( new line )
+
+            if( $isAscending ){
+
+                krsort( $rows );    //  reverse array keys
+
+            }
+
+            return implode( '<br/>', $rows );   //  glue all parts together with html break line code
+
+        } else {
+
+            return false;
+
+        }
 
     }
 
