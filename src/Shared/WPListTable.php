13a12b4f741c7a3c58aacebe4b9b81751b0800f7
dualjack <jakubkuranda@gmail.com>
2017-06-21 22:16:09 +0200

    added filter for wp list table search box pagination support

--- src/Shared/WPListTable.php
+++ src/Shared/WPListTable.php
@@ -24,6 +24,12 @@ class WPListTable extends WP_List_Table {
 
         $this->app = $app;
 
+        //  ----------------------------------------
+        //  Actions and filters
+        //  ----------------------------------------
+
+        add_filter( 'set_url_scheme',   array( $this, '_f_addSearchArgsToUrl') );
+
     }
 
     /**
@@ -52,6 +58,33 @@ class WPListTable extends WP_List_Table {
         ob_start();
         parent::search_box( $text, $inputId );
         return ob_get_clean();
+
+    }
+
+    //  ================================================================================
+    //  FILTERS
+    //  ================================================================================
+
+    /**
+     * Filters the resulting URL.
+     * This filter is used by Wordpress's WP_List_Table
+     * while generating pagination buttons.
+     *
+     * @param string $url - The complete URL including scheme and path.
+     *
+     * @filter set_url_scheme
+     * @see WP_List_Table::pagination()
+     */
+    public function _f_addSearchArgsToUrl( $url ) {
+
+        if( isset( $_POST['s'] ) ){
+
+            $url = add_query_arg( 's', $_POST['s'], $url );      //  adds search argument
+            $url = remove_query_arg( 'paged', $url );       //  removes current paged argument, because we handle NEW search
+
+        }
+
+        return $url;
 
     }
 
