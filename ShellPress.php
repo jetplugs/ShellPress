9ca4757387cf55ad9cbaa8049f92a6036119e4dd
dualjack <jakubkuranda@gmail.com>
2017-07-13 13:41:30 +0200

    property mainPluginFile is now private

--- ShellPress.php
+++ ShellPress.php
@@ -29,7 +29,7 @@ class ShellPress {
 	public static $log;
 
     /** @var string */
-    protected static $mainPluginFile;
+    private static $mainPluginFile;
 
     /** @var string */
     private static $pluginPrefix;
