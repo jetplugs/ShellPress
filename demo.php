30176d95cefeafdd59abf8a5d7414c1aad56b60d
dualjack <jakubkuranda@gmail.com>
2019-01-15 21:41:33 +0100

    Add: Plugin Demo.

---
+++ demo.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name:     ShellPress Demo
+ * Description:     Development demo for ShellPress framework.
+ * Version:         1.0.0
+ * Plugin URI:      https://themastercut.co
+ * Author:          TheMasterCut.co
+ * License:         GPL-2.0+
+ * Text Domain:     shellpress
+ * Domain Path:     /langugages
+ */
+
+//  ----------------------------------------
+//  Requirements
+//  ----------------------------------------
+
+require __DIR__ . '/vendor/autoload.php';   //  Composer
+
+$requirementChecker = new ShellPress_RequirementChecker();
+
+$checkPHP   = $requirementChecker->checkPHPVersion( '5.3', 'ShellPress Demo requires PHP version >= 5.3' );
+$checkWP    = $requirementChecker->checkWPVersion( '4.8', 'ShellPress Demo requires WP version >= 4.8' );
+
+if( ! $checkPHP || ! $checkWP ) return;
+
+//  ----------------------------------------
+//  ShellPress
+//  ----------------------------------------
+
+\shellpress\v1_3_4\demo\App::initShellPress( __FILE__, 'shellpress', '1.0.0' );   //  <--- Remember to always change version here
---
+++ demo/App.php
@@ -0,0 +1,22 @@
+<?php
+namespace shellpress\v1_3_4\demo;
+
+use shellpress\v1_3_4\ShellPress;
+
+/**
+ * Date: 15.01.2019
+ * Time: 21:40
+ */
+
+class App extends ShellPress {
+
+	/**
+	 * Called automatically after core is ready.
+	 *
+	 * @return void
+	 */
+	protected function onSetUp() {
+		// TODO: Implement onSetUp() method.
+	}
+
+}
