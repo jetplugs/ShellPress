d369566a603e560b4d520c0433d5e97ed2ee9c7d
themastercut <hello@themastercut.co>
2019-05-28 18:03:33 +0200

    Add: License manager stub.

---
+++ src/Shared/Components/IComponentLicenseManager.php
@@ -0,0 +1,14 @@
+<?php
+namespace shellpress\v1_3_72\src\Shared\Components;
+
+/**
+ * @author jakubkuranda@gmail.com
+ * Date: 28.05.2019
+ * Time: 17:55
+ */
+
+abstract class IComponentLicenseManager extends IComponent {
+
+
+
+}
