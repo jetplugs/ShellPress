d2aecd9e4c3b5449cad76647caf1249047a79667
dualjack <jakubkuranda@gmail.com>
2019-10-30 20:20:23 +0100

    Refactor: Version.

--- src/Shared/Front/Models/HtmlElement.php
+++ src/Shared/Front/Models/HtmlElement.php
@@ -1,5 +1,5 @@
 <?php
-namespace shellpress\v1_3_78\src\Shared\Front\Models;
+namespace shellpress\v1_3_79\src\Shared\Front\Models;
 
 /**
  * @author jakubkuranda@gmail.com
--- src/Shared/RestModels/UniversalFrontResponse.php
+++ src/Shared/RestModels/UniversalFrontResponse.php
@@ -1,5 +1,5 @@
 <?php
-namespace shellpress\v1_3_78\src\Shared\RestModels;
+namespace shellpress\v1_3_79\src\Shared\RestModels;
 
 /**
  * @author jakubkuranda@gmail.com
